80bff938bb28a8af678452f453845fde8e1dae4b
Kiss György <kissgyorgy@me.com>
2017-10-20 19:22:39 +0200

    Prepare module to use in a web environment

--- gerrit_slack_bot.py
+++ gerrit_slack_bot.py
@@ -5,13 +5,17 @@ import slack
 import gerrit
 
 
-def main():
+def get_changes_and_submit_to_slack():
     gerrit_change_list = gerrit.get(gerrit.CHANGES_URL)
     gerrit_changes = [gerrit.Change(c) for c in gerrit_change_list]
-    if not gerrit_changes:
-        return 0
-    res = slack.post(gerrit_changes)
-    return 0 if res.ok else 1
+    return slack.post(gerrit_changes) if gerrit_changes else None
+
+
+def main():
+    res = get_changes_and_submit_to_slack()
+    if res is None or not res.ok:
+        return 1
+    return 0
 
 
 if __name__ == '__main__':
