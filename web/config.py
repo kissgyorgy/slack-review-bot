c8b94ab678b4120d4b72f9011fbe79c819663c4c
Kiss György <kissgyorgy@me.com>
2018-01-20 19:53:22 +0100

    Delete dead-end config possibilities

--- web/config.py
+++
@@ -1,8 +0,0 @@
-from nnx.config.node import Node
-
-
-class SlackBotSetting(Node):
-    timespec = SimpleProperty(str)
-    channel = SimpleProperty(str)
-    gerrit_query = SimpleProperty(str)
-    slack_webhook_url = SimpleProperty(str)
