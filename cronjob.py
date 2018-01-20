a8f8f3d0f601600eb9ebdb826fc2e16c787cac06
Kiss György <kissgyorgy@me.com>
2018-01-20 19:58:12 +0100

    Simple json configuration
    Every channel will get a json file and the crontab mule will load this
    on start. When a new json is created, or an exsiting is modified, the
    mule will be reloaded.

--- cronjob.py
+++ cronjob.py
@@ -1,42 +1,79 @@
 import sys
 import time
+import glob
 import json
 import datetime as dt
-
-import uwsgi
 from croniter import croniter
 
 
-def read_state():
-    size = uwsgi.sharedarea_readlong(0, 0)
-    state = uwsgi.sharedarea_read(0, 8, size)
-    print('STATE', type(state), len(state))
-    return json.loads(state)
+def load_config():
+    config = {}
+
+    for fname in glob.glob('config/*.json'):
+        print('Processing file', fname)
+        with open(fname) as fp:
+            channel_config = json.load(fp)
+        channel = channel_config['slack']['incoming_webhook']['channel']
+        config[channel] = channel_config
+
+    return config
+
+
+def init_crontabs(config):
+    crontabs = {}
+    base = dt.datetime.now()
+    for channel, conf in config.items():
+        cron = croniter(conf['crontab'], base)
+        next_dt = cron.get_next(dt.datetime)
+        crontabs[channel] = [cron, next_dt]
+    return crontabs
+
+
+# def get_all_next_dts(crontabs):
+#     for channel, cron in crontabs.items():
+#         next_dt = cron.get_next(dt.datetime)
+#         crontabs[channel][1] = next_dt
+#     return crontabs
+
+
+# def check_dts(crontabs, now):
+#     for channel, cron, next_dt in crontabs.items():
+#         if next_dt == rounded_now:
+
+
+# def get_earliest_dt(crontabs):
+#     next_dt = None
+#     for channel, cron in crontabs.items():
+#         current_dt = cron.get_next(dt.datetime)
+#         if next_dt is None or current_dt < next_dt:
+#             next_dt = current_dt
+#             next_channel = channel
+
+#     print('Earliest cron entry:', next_channel, next_dt)
+#     return next_channel, next_dt
 
 
 def main():
-    print('Started main')
-    time.sleep(15)
-    json_state = read_state()
-    print('Shared state:', json_state)
-    base = dt.datetime.now()
-    # cron = croniter('0 9-18 * * mon-fri', base)
-    cron = croniter('*/1 * * * *', base)
-    print('Getting next_dt')
-    next_dt = cron.get_next(dt.datetime)
+    print('Crontab started')
+    config = load_config()
+    print('Config:', config)
+    crontabs = init_crontabs(config)
+    # channel, next_dt = get_earliest_dt(crontabs)
 
-    print('Starting cycle')
     while True:
-        state = uwsgi.sharedarea_read(0, 0, 10)
         now = dt.datetime.now()
         rounded_now = now.replace(second=0, microsecond=0)
-        if next_dt == rounded_now:
-            next_dt = cron.get_next(dt.datetime)
-            print('Stimmt, state:', read_state())
+        for channel, crontup in crontabs.items():
+            cron, next_dt = crontup
+            if next_dt == rounded_now:
+                print('Sending now to', channel)
+                next_dt = cron.get_next(dt.datetime)
+                crontabs[channel][1] = next_dt
+        # if next_dt == rounded_now:
+            # channel, next_dt = get_earliest_dt(crontabs)
         print('Next', next_dt, 'now:', now)
         time.sleep(5)
 
 
 if __name__ == '__main__':
-    print('NAME IS MAIN')
     sys.exit(main())
