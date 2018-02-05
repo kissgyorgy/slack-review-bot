c0b3761d77b9243ed559cdeba7fe42124bf3d92c
Kiss György <kissgyorgy@me.com>
2018-02-05 23:49:54 +0100

    refactor bot.py (former cronjob.py and gerrit_slack_bot.py)

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3.6
 
 import sys
+import time
 import textwrap
+import datetime as dt
+from croniter import croniter
 import slack
 import gerrit
+import database
 
 
 class PostableChange:
@@ -17,6 +21,10 @@ class PostableChange:
     @property
     def ver(self):
         return self._gerrit_change.verified
+
+    @property
+    def url(self):
+        return self._gerrit_change.url
 
     @property
     def username(self):
@@ -57,33 +65,71 @@ class PostableChange:
         else:
             return '#EC1313'
 
+    def full_message(self):
+        text = f'CR: {self.code_review_icon} V: {self.verified_icon} - {self.username}: {self.subject}'
+        # Slack wraps lines around 80? width, so if we cut out here explicitly,
+        # every patch will fit in one line
+        return textwrap.shorten(text, width=80, placeholder=' …')
 
-def run(gerrit_url, gerrit_query, slack_webhook_url, slack_channel=None):
-    gerrit_changes = gerrit.get_changes(gerrit_url, gerrit_query)
-    changes = [PostableChange(c) for c in gerrit_changes]
-    if not changes:
+
+class CronJob:
+    def __init__(self, crontab, gerrit_url, gerrit_query, slack_webhook_url, slack_channel):
+        self._crontab = crontab
+        # This way, we will miss this very minute at startup to avoid sending the same message twice.
+        self._cron = croniter(crontab, start_time=dt.datetime.now())
+        self.calc_next_run()
+
+        self._gerrit = gerrit.Client(gerrit_url, gerrit_query)
+        self._slack_channel = slack.Channel(slack_webhook_url, slack_channel)
+
+    def __str__(self):
+        return f'{self._crontab}: {self._gerrit.query} -> {self._slack_channel}'
+
+    def __repr__(self):
+        return f"CronJob(crontab='{self._crontab}', query='{self._gerrit.query}', channel='{self._slack_channel}')"
+
+    def calc_next_run(self):
+        self.next_run = self._cron.get_next(dt.datetime)
+
+    def run(self):
+        changes = [PostableChange(c) for c in self._gerrit.get_changes()]
+        if not changes:
+            return True
+
+        summary_text = f'{len(changes)} patch vár review-ra:'
+        summary_link = slack.make_link(self._gerrit.changes_url, summary_text)
+        attachments = [slack.make_attachment(c.color, c.full_message(), c.url) for c in changes]
+
+        res = self._slack_channel.post(summary_link, attachments)
+        if not res.ok:
+            print(f'{res.status_code} error requesting {res.url} for channel {self._channel}:',
+                  res.text, file=sys.stderr)
+            return False
+
         return True
 
-    slack_client = slack.SlackClient(slack_webhook_url, slack_channel)
-    summary_link = _make_summary(gerrit_url, gerrit_query, changes)
-    attachments = [slack.make_attachment(c.color, _make_message(c), c.url) for c in changes]
 
-    res = slack_client.post(summary_link, attachments)
-    if not res.ok:
-        print(res.status_code, 'error:', res.text, file=sys.stderr)
-        return False
+def main():
+    db = database.Database()
+    gerrit_url = db.load_environment().GERRIT_URL
+    crontab = [CronJob(row['crontab'], gerrit_url, row['gerrit_query'], row['webhook_url'], row['channel'])
+               for row in db.load_crontabs()]
+    db.close()
+    print(crontab)
 
-    return True
+    while True:
+        now = dt.datetime.now()
+        rounded_now = now.replace(second=0, microsecond=0)
+        print(now, 'Checking crontabs to run...')
+
+        for job in crontab:
+            if job.next_run == rounded_now:
+                print('Running job...', job)
+                job.run()
+                job.calc_next_run()
+
+        time.sleep(5)
 
 
-def _make_summary(gerrit_url, gerrit_query, gerrit_changes):
-    gerrit_changes_url = gerrit.make_changes_url(gerrit_url, gerrit_query)
-    link_text = f'{len(gerrit_changes)} patch vár review-ra:'
-    return slack.make_link(gerrit_changes_url, link_text)
-
-
-def _make_message(change):
-    text = f'CR: {change.code_review_icon} V: {change.verified_icon} - {change.username}: {change.subject}'
-    # Slack wraps lines around 80? width, so if we cut out here explicitly,
-    # every patch will fit in one line
-    return textwrap.shorten(text, width=80, placeholder=' …')
+if __name__ == '__main__':
+    main()
