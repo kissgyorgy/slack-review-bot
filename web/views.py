585cda13be2ced50e9bf5b8fc05a4baad9fdbb44
Kiss György <kissgyorgy@me.com>
2018-02-25 11:00:19 +0100

    Reorganize flask app a little bit. Move to a package.
    We need to use uwsgi module option now.

--- web/views.py
+++ web/views.py
@@ -4,28 +4,7 @@ import uwsgi
 import slack
 from database import Database, Crontab
 from bot import CronTime, MuleMessage
-
-
-db = Database()
-env = db.load_environment()
-db.close()
-
-app = Flask(__name__)
-app.secret_key = env.SECRET_KEY
-
-slack_app = slack.App(env.SLACK_CLIENT_ID, env.SLACK_CLIENT_SECRET, env.SLACK_REDIRECT_URI)
-slack_api = slack.Api(env.BOT_ACCESS_TOKEN)
-
-
-class Alert:
-    PRIMARY = 'primary'
-    SECONDARY = 'secondary'
-    SUCCESS = 'success'
-    DANGER = 'danger'
-    WARNING = 'warning'
-    INFO = 'info'
-    LIGHT = 'light'
-    DARK = 'dark'
+from . import app, slack_app, slack_api, Alert
 
 
 @app.before_request
