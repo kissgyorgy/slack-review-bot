2f796c91c81817712c0a462e6a5778342e83a667
Kiss György <kissgyorgy@me.com>
2018-02-26 03:42:47 +0100

    Django migrations for the models

--- web/manage.py
+++ web/manage.py
@@ -12,4 +12,8 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # slack, gerrit and bot modules are one directory above
+    sys.path.append('..')
+
     execute_from_command_line(sys.argv)
