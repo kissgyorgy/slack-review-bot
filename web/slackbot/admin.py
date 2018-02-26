2ada053d445a1217b22adcdca65579903ba00229
Kiss György <kissgyorgy@me.com>
2018-02-26 02:55:49 +0100

    Implement automatic channel_id lookup from slack

--- web/slackbot/admin.py
+++ web/slackbot/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Crontab, SentMessage
 
 
-admin.site.register(Crontab)
+class CrontabAdmin(admin.ModelAdmin):
+    readonly_fields = ('channel_id',)
+
+    class Meta:
+        model = Crontab
+
+
+admin.site.register(Crontab, CrontabAdmin)
 admin.site.register(SentMessage)
--- web/slackbot/models.py
+++ web/slackbot/models.py
@@ -1,11 +1,13 @@
 import datetime as dt
 from croniter import croniter
+from constance import config
 from django.db import models
+import slack
 
 
 class Crontab(models.Model):
     channel_name = models.CharField(max_length=100)
-    channel_id = models.CharField(max_length=30)
+    channel_id = models.CharField(max_length=30, blank=True)
     gerrit_query = models.CharField(max_length=255)
     crontab = models.CharField(max_length=255)
 
@@ -22,6 +24,12 @@ class Crontab(models.Model):
 
     def __str__(self):
         return f'{self.crontab}: {self.gerrit_query} -> {self.channel_name}'
+
+    def save(self, *args, **kwargs):
+        slack_api = slack.Api(config.BOT_ACCESS_TOKEN)
+        # TODO: cache the channels.info result for faster lookup
+        self.channel_id = slack_api.get_channel_id(self.channel_name)
+        return super().save(*args, **kwargs)
 
     def calc_next(self):
         self.next = self._cron.get_next(dt.datetime)
